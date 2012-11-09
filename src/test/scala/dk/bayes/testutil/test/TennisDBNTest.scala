46aa3f271253d7b0cae8f05ccea7808e8ea4e10e
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2012-11-09 19:01:19 +0000

    Implementing Tennis DBN.

---
+++ src/test/scala/dk/bayes/testutil/test/TennisDBNTest.scala
@@ -0,0 +1,29 @@
+package dk.bayes.testutil.test
+
+import org.junit._
+import Assert._
+import dk.bayes.infer.LoopyBP
+import dk.bayes.testutil.TennisDBN._
+import dk.bayes.testutil.AssertUtil._
+import dk.bayes.factor._
+
+class TennisDBNTest {
+
+  val tennisGraph = createTennisClusterGraph()
+  val loopyBP = LoopyBP(tennisGraph)
+
+  @Test def marginal_for_grade_given_sat_is_high {
+
+    loopyBP.setEvidence(match1v2Time0.id, 0)
+    loopyBP.setEvidence(match1v2Time1.id, 0)
+    loopyBP.setEvidence(match2v3Time1.id, 1)
+    loopyBP.setEvidence(match1v3Time2.id, 1)
+    loopyBP.setEvidence(match2v3Time2.id, 0)
+
+    loopyBP.calibrate()
+
+    val match1v2Time2Marginal = loopyBP.marginal(match1v2Time2.id)
+
+    assertFactor(Factor(Var(12, 2), Array(0.5407, 0.4592)), match1v2Time2Marginal, 0.0001)
+  }
+}
