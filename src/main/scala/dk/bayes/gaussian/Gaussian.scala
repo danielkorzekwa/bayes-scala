11b9f5bb01d3ccc25cba978ee464024e9a4f2d2c
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2013-01-30 19:06:12 +0000

    Implementing Univariate Gaussian

--- src/main/scala/dk/bayes/gaussian/Gaussian.scala
+++ src/main/scala/dk/bayes/gaussian/Gaussian.scala
@@ -4,12 +4,15 @@ import Math._
 
 /**
  * Univariate Gaussian Distribution.
- * 
+ *
  * @author Daniel Korzekwa
+ *
+ * @param mu Mean
+ * @param sigma Variance
  */
-case class Gaussian(mean: Double, variance: Double) {
+case class Gaussian(mu: Double, sigma: Double) {
 
-  def pdf(x: Double) = normConstant * exp(-pow(x - mean, 2) / (2 * variance))
+  def pdf(x: Double) = normConstant * exp(-pow(x - mu, 2) / (2 * sigma))
 
-  def normConstant(): Double = 1 / sqrt(2 * Pi * variance)
+  def normConstant(): Double = 1 / sqrt(2 * Pi * sigma)
 }
