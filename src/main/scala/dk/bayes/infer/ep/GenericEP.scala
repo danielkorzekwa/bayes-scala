211c246780bf270a0d47b976f03fc19457c0d3dc
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2013-04-12 21:33:39 +0100

    Implementing Expectation Propagation on a factor graph.

--- src/main/scala/dk/bayes/infer/ep/GenericEP.scala
+++ src/main/scala/dk/bayes/infer/ep/GenericEP.scala
@@ -17,12 +17,33 @@ case class GenericEP(factorGraph: FactorGraph) extends EP {
   def calibrate() = {
 
     val nodes = factorGraph.getNodes()
-
     for (node <- nodes) {
       node match {
-        case FactorNode(factor) =>
-        case VarNode(varId) =>
+        case node: FactorNode => sendFactorMessage(node)
+        case node: VarNode => sendVariableMessage(node)
       }
+    }
+  }
+
+  private def sendFactorMessage(factorNode: FactorNode) {
+    val inMsgs = factorNode.getGates().map(g => g.getEndGate.getMessage())
+    for (gate <- factorNode.getGates()) {
+
+      val marginalVarId = gate.getMessage.getVariableIds.head
+      val marginalNode = factorNode.factor.productMarginal(marginalVarId, inMsgs)
+      val newMessage = marginalNode / gate.getEndGate.getMessage
+
+      gate.setMessage(newMessage)
+    }
+  }
+
+  private def sendVariableMessage(varNode: VarNode) {
+    val inMsgs = varNode.getGates().map(g => g.getEndGate.getMessage())
+    val marginalFactor = inMsgs.reduceLeft((msg1, msg2) => msg1 * msg2)
+    for (gate <- varNode.getGates()) {
+
+      val newMessage = marginalFactor / gate.getEndGate.getMessage
+      gate.setMessage(newMessage)
     }
   }
 
@@ -32,7 +53,7 @@ case class GenericEP(factorGraph: FactorGraph) extends EP {
 
     val gates = varNode.getGates()
 
-    val inMsgs = gates.map(g => g.getEndGate.message)
+    val inMsgs = gates.map(g => g.getEndGate.getMessage())
 
     val marginalFactor = inMsgs.reduceLeft((msg1, msg2) => msg1 * msg2)
 
