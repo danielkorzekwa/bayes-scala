aa0a3b19256f2d38616bd61b846fa6c5cef767c7
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2013-04-12 21:16:38 +0100

    Implementing TruncatedGaussianFactor.

--- src/main/scala/dk/bayes/model/factor/TruncGaussianFactor.scala
+++ src/main/scala/dk/bayes/model/factor/TruncGaussianFactor.scala
@@ -11,13 +11,28 @@ package dk.bayes.model.factor
  */
 case class TruncGaussianFactor(gaussianVarId: Int, truncVarId: Int, truncValue: Double) extends Factor {
 
-  def getVariableIds(): Seq[Int] = throw new UnsupportedOperationException("Not implemented yet")
+  def getVariableIds(): Seq[Int] = Vector(gaussianVarId, truncVarId)
 
-  def marginal(varId: Int): Factor = throw new UnsupportedOperationException("Not implemented yet")
+  def marginal(varId: Int): Factor = {
 
-  def withEvidence(varId: Int, varValue: AnyVal): TableFactor = throw new UnsupportedOperationException("Not implemented yet")
+    val marginalFactor = varId match {
+      case `gaussianVarId` => GaussianFactor(varId, Double.NaN, Double.PositiveInfinity)
+      case `truncVarId` => TableFactor(Vector(varId), Vector(2), Vector(1d, 1d))
+      case _ => throw new IllegalArgumentException("Unknown variable id: " + varId)
+    }
+
+    marginalFactor
+  }
+
+  def productMarginal(varId: Int, factors: Seq[Factor]): Factor = {
+    throw new UnsupportedOperationException("Not implemented yet")
+  }
+
+  def withEvidence(varId: Int, varValue: AnyVal): TruncGaussianFactor = throw new UnsupportedOperationException("Not implemented yet")
 
   def getValue(assignment: (Int, AnyVal)*): Double = throw new UnsupportedOperationException("Not implemented yet")
 
   def *(factor: Factor): Factor = throw new UnsupportedOperationException("Not implemented yet")
+
+  def /(that: Factor): Factor = throw new UnsupportedOperationException("Not implemented yet")
 }
