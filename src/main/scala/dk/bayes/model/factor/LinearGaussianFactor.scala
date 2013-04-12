69fb1596cef487dcf321c1b999d7cc3abee0fc44
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2013-04-12 21:10:31 +0100

    Implementing LinearGaussianFactor

--- src/main/scala/dk/bayes/model/factor/LinearGaussianFactor.scala
+++ src/main/scala/dk/bayes/model/factor/LinearGaussianFactor.scala
@@ -1,5 +1,9 @@
 package dk.bayes.model.factor
 
+import dk.bayes.gaussian.CanonicalGaussian
+import dk.bayes.gaussian.CanonicalGaussian
+import dk.bayes.gaussian.Linear._
+
 /**
  * This class represents a factor for a Linear Gaussian Distribution. N(ax + b,v)
  *
@@ -15,11 +19,32 @@ case class LinearGaussianFactor(parentVarId: Int, varId: Int, a: Double, b: Doub
 
   def getVariableIds(): Seq[Int] = Vector(parentVarId, varId)
 
-  def marginal(varId: Int): Factor = throw new UnsupportedOperationException("Not implemented yet")
+  def marginal(varId: Int): Factor = GaussianFactor(varId, Double.NaN, Double.PositiveInfinity)
 
-  def withEvidence(varId: Int, varValue: AnyVal): TableFactor = throw new UnsupportedOperationException("Not implemented yet")
+  def productMarginal(marginalVarId: Int, factors: Seq[Factor]): GaussianFactor = {
+    factors.foreach(f => require(f.isInstanceOf[GaussianFactor], "Linear Gaussian factor cannot be multiplied by non gaussian factor:" + f))
+    require(!factors.isEmpty, "Linear Gaussian factor cannot be multiplied by an empty list of factors")
+    factors.asInstanceOf[Seq[GaussianFactor]].foreach(f => require(f.varId == parentVarId || f.varId == varId, "Incorrect factor variable id: " + f.varId))
+
+    val factorGaussian = CanonicalGaussian(Array(parentVarId, varId), Matrix(a), b, v)
+    val otherFactorGaussians = factors.asInstanceOf[Seq[GaussianFactor]].map(f => CanonicalGaussian(f.varId, f.m, f.v))
+    val factorProduct = otherFactorGaussians.foldLeft(factorGaussian)((f1, f2) => f1 * f2)
+
+    val marginalGaussian = marginalVarId match {
+      case `parentVarId` => factorProduct.marginalise(varId)
+      case `varId` => factorProduct.marginalise(parentVarId)
+      case _ => throw new IllegalArgumentException("Incorrect marginal variable id")
+    }
+
+    val marginalFactor = GaussianFactor(marginalVarId, marginalGaussian.toGaussian.m, marginalGaussian.toGaussian.v)
+    marginalFactor
+  }
+
+  def withEvidence(varId: Int, varValue: AnyVal): LinearGaussianFactor = throw new UnsupportedOperationException("Not implemented yet")
 
   def getValue(assignment: (Int, AnyVal)*): Double = throw new UnsupportedOperationException("Not implemented yet")
 
   def *(factor: Factor): Factor = throw new UnsupportedOperationException("Not implemented yet")
+
+  def /(that: Factor): Factor = throw new UnsupportedOperationException("Not implemented yet")
 }
