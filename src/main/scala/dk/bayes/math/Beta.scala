5649726e8b3b669df20e70c3e49689eae7ba492f
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2014-05-01 22:29:33 +0100

    Implementing Beta distr.

--- src/main/scala/dk/bayes/math/Beta.scala
+++ src/main/scala/dk/bayes/math/Beta.scala
@@ -13,3 +13,13 @@ case class Beta(alpha: Double, beta: Double) {
   def mean(): Double = alpha / (alpha + beta)
   def variance(): Double = alpha * beta / (pow(alpha + beta, 2) * (alpha + beta + 1))
 }
+
+object Beta {
+  
+  def fromMeanAndVariance(m: Double, v: Double): Beta = {
+    val alpha = ((1 - m) / v - 1d / m) * (m * m)
+    val beta = alpha * (1d / m - 1)
+
+    new Beta(alpha, beta)
+  }
+}
--- src/test/scala/dk/bayes/math/BetaTest.scala
+++ src/test/scala/dk/bayes/math/BetaTest.scala
@@ -9,5 +9,10 @@ class BetaTest {
     val beta = Beta(3.2, 6.5)
     assertEquals(0.3298, beta.mean, 0.0001)
     assertEquals(0.0206, beta.variance, 0.0001)
+
+    val beta2 = Beta.fromMeanAndVariance(beta.mean, beta.variance)
+
+    assertEquals(beta.mean, beta2.mean, 0.0001)
+    assertEquals(beta.variance, beta2.variance, 0.00001)
   }
 }
