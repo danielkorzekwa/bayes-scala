1d10302d516edcd1f32c20fb4accc8808e79eab6
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2015-04-10 17:20:22 +0100

    Supporting inference for Multivariate Gaussian variable with non zero mean.

--- src/main/scala/dk/bayes/dsl/variable/gaussian/multivariate/inferMultivariateGaussianSimplest.scala
+++ src/main/scala/dk/bayes/dsl/variable/gaussian/multivariate/inferMultivariateGaussianSimplest.scala
@@ -2,6 +2,7 @@ package dk.bayes.dsl.variable.gaussian.multivariate
 
 import dk.bayes.dsl.InferEngine
 import dk.bayes.dsl.variable.gaussian.multivariatelinear.MultivariateLinearGaussian
+import dk.bayes.math.gaussian.canonical.DenseCanonicalGaussian
 
 object inferMultivariateGaussianSimplest extends InferEngine[MultivariateGaussian, MultivariateGaussian] {
 
@@ -19,16 +20,32 @@ object inferMultivariateGaussianSimplest extends InferEngine[MultivariateGaussia
     (child.getParents().size == 1 && child.getParents()(0).eq(x)) &&
       !child.hasChildren &&
       child.b.size == x.m.size &&
-      child.b.toArray.sum == 0 &&
-      child.yValue.isDefined &&
-      x.m.toArray.sum == 0
+      child.yValue.isDefined 
+     
   }
 
   def infer(x: MultivariateGaussian): MultivariateGaussian = {
     val child = x.getChildren.head.asInstanceOf[MultivariateLinearGaussian]
 
-    val posteriorVar = (x.v.inv + child.a.t * child.v.inv * child.a).inv
-    val posteriorMean = posteriorVar * (child.a.t * child.v.inv * child.yValue.get)
+    val xx = DenseCanonicalGaussian(x.m,x.v)
+    val yy = DenseCanonicalGaussian(child.a,child.b,child.v)
+    
+    val zz = (xx.extend(6,0)*yy).withEvidence(5, 9).withEvidence(4, 4).withEvidence(3, 1)
+    println(zz.mean)
+    println(zz.variance)
+    
+    val xVInv = x.v.inv
+    val childVInv = child.v.inv
+    
+     val posteriorVar = (xVInv + child.a.t * childVInv * child.a).inv
+    val posteriorMean = posteriorVar * (child.a.t * childVInv * (child.yValue.get-child.b) + xVInv*x.m)
+   
+//    val posteriorVar = (x.v.inv + child.a.t * child.v.inv * child.a).inv
+//    val posteriorMean = posteriorVar * (child.a.t * child.v.inv * child.yValue.get)
+    
+    println(posteriorMean)
+    println(posteriorVar)
+    
     new MultivariateGaussian(posteriorMean, posteriorVar)
   }
 }
--- src/test/scala/dk/bayes/dsl/demo/GaussianProcessRegressionTest.scala
+++ src/test/scala/dk/bayes/dsl/demo/GaussianProcessRegressionTest.scala
@@ -10,7 +10,7 @@ import dk.bayes.dsl.infer
 import dk.bayes.testutil.AssertUtil._
 class GaussianProcessRegressionTest {
 
-  @Test def test {
+  @Test def test = {
 
     val fMean = Matrix(0, 0, 0)
     val x = Matrix(1, 2, 3)
@@ -24,6 +24,23 @@ class GaussianProcessRegressionTest {
     val fPosterior = infer(f)
 
     assertTrue("fPosterior mean is incorrect: " + fPosterior.m, Matrix(0.878, 4.407, 8.614).isIdentical(fPosterior.m, tol = 0.001))
+    assertTrue("fPosterior diag variance is incorrect: " + fPosterior.v.extractDiag, Matrix(0.588, 0.465, 0.588).isIdentical(fPosterior.v.extractDiag, tol = 0.001))
+  }
+
+  @Test def test_non_zero_mean = {
+
+    val fMean = Matrix(1, 1, 1)
+    val x = Matrix(1, 2, 3)
+    val covFunc = CovSEiso(sf = log(7.5120), ell = log(2.1887))
+    val fVar = covFunc.cov(x)
+    val f = Gaussian(fMean, fVar) //f variable
+
+    val yVar = pow(0.81075, 2) * Matrix.identity(3)
+    val y = Gaussian(f, yVar, yValue = Matrix(1.0, 4, 9)) //y variable
+
+    val fPosterior = infer(f)
+
+    assertTrue("fPosterior mean is incorrect: " + fPosterior.m, Matrix(0.897, 4.384, 8.633).isIdentical(fPosterior.m, tol = 0.001))
     assertTrue("fPosterior diag variance is incorrect: " + fPosterior.v.extractDiag, Matrix(0.588, 0.465, 0.588).isIdentical(fPosterior.v.extractDiag, tol = 0.001))
   }
 }
